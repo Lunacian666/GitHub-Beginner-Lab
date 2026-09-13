41ec3239283bb2859c85c32d35289082a864b33e
Jeremy Kobi Butor <kobibutor03@gmail.com>
2026-09-13 17:03:03 +0800

    Implement student search feature (#2)

--- StudentProfile/StudentProfile/Form1.Designer.cs
+++ StudentProfile/StudentProfile/Form1.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             lblstudentProfile = new Label();
+            txtID = new TextBox();
+            txtName = new TextBox();
+            lblName = new Label();
+            lblID = new Label();
+            bttnSearch = new Button();
+            lblResult = new Label();
             SuspendLayout();
             // 
             // lblstudentProfile
@@ -42,11 +48,69 @@
             lblstudentProfile.Text = "Student Profile - GitHub-Beginner-Lab";
             lblstudentProfile.Click += label1_Click;
             // 
+            // txtID
+            // 
+            txtID.Location = new Point(231, 118);
+            txtID.Name = "txtID";
+            txtID.Size = new Size(100, 23);
+            txtID.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(466, 118);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(100, 23);
+            txtName.TabIndex = 2;
+            // 
+            // lblName
+            // 
+            lblName.AutoSize = true;
+            lblName.Location = new Point(466, 100);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(78, 15);
+            lblName.TabIndex = 3;
+            lblName.Text = "Enter a Name";
+            // 
+            // lblID
+            // 
+            lblID.AutoSize = true;
+            lblID.Location = new Point(231, 100);
+            lblID.Name = "lblID";
+            lblID.Size = new Size(57, 15);
+            lblID.TabIndex = 4;
+            lblID.Text = "Enter a ID";
+            // 
+            // bttnSearch
+            // 
+            bttnSearch.Location = new Point(364, 136);
+            bttnSearch.Name = "bttnSearch";
+            bttnSearch.Size = new Size(75, 23);
+            bttnSearch.TabIndex = 5;
+            bttnSearch.Text = "Search";
+            bttnSearch.UseVisualStyleBackColor = true;
+            bttnSearch.Click += bttnSearch_Click;
+            // 
+            // lblResult
+            // 
+            lblResult.AutoSize = true;
+            lblResult.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblResult.Location = new Point(231, 197);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(69, 30);
+            lblResult.TabIndex = 6;
+            lblResult.Text = "Result";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 164);
+            ClientSize = new Size(800, 288);
+            Controls.Add(lblResult);
+            Controls.Add(bttnSearch);
+            Controls.Add(lblID);
+            Controls.Add(lblName);
+            Controls.Add(txtName);
+            Controls.Add(txtID);
             Controls.Add(lblstudentProfile);
             Name = "Form1";
             Text = "Form1";
@@ -57,5 +121,11 @@
         #endregion
 
         private Label lblstudentProfile;
+        private TextBox txtID;
+        private TextBox txtName;
+        private Label lblName;
+        private Label lblID;
+        private Button bttnSearch;
+        private Label lblResult;
     }
 }
